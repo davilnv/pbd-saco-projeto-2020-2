41824a4dae5228c9c536024a1d308820ed587a7c
Davi Lima <davilima871@gmail.com>
2021-11-19 19:57:48 -0300

    Criado os outros endpoints

--- backend/saco/saco/urls.py
+++ backend/saco/saco/urls.py
@@ -25,6 +25,18 @@ from tool.api import viewsets as toolviewsets
 route = routers.DefaultRouter()
 
 route.register(r'os', toolviewsets.OrdemServicoViewSet, basename='os')
+route.register(r'cliente', toolviewsets.ClienteViewSet, basename='cliente')
+route.register(r'clienteveiculo', toolviewsets.ClienteVeiculoViewSet, basename='clienteveiculo')
+route.register(r'empresa', toolviewsets.EmpresaViewSet, basename='empresa')
+route.register(r'endereco', toolviewsets.EnderecoViewSet, basename='endereco')
+route.register(r'funcionario', toolviewsets.FuncionarioViewSet, basename='funcionario')
+route.register(r'item', toolviewsets.ItemViewSet, basename='item')
+route.register(r'nfservico', toolviewsets.NfservicoViewSet, basename='nfservico')
+route.register(r'ordem_servico', toolviewsets.Ordem_ServicoViewSet, basename='ordem_servico')
+route.register(r'servico', toolviewsets.ServicoViewSet, basename='servico')
+route.register(r'servicoitem', toolviewsets.ServicoItemViewSet, basename='servicoitem')
+route.register(r'usuario', toolviewsets.UsuarioViewSet, basename='usuario')
+route.register(r'veiculo', toolviewsets.VeiculoViewSet, basename='veiculo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
