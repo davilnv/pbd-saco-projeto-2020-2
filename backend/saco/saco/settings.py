2489bd69c749bb78f2962ca11901f8d76c16f1c9
Davi Lima <davilima871@gmail.com>
2021-11-25 20:31:56 -0300

    Problema no CORS

--- backend/saco/saco/settings.py
+++ backend/saco/saco/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)_r2&4%p9fgrt-(@4u*&%cc6^*^yr*m&m-xc^=9vk2-sv4qh4p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tool'
+    'tool',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
 ]
 
 ROOT_URLCONF = 'saco.urls'
